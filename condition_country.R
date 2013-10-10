b8aa1b97858c176c3dd9bd7d3fc7d774a2aeb5d8
Rajarshi Guha <rajarshi.guha@gmail.com>
2013-10-10 15:35:48 -0400

    Added code to color based on cumulative counts

--- condition_country.R
+++ condition_country.R
@@ -6,15 +6,23 @@ d <- read.csv('condition_country.csv', header=TRUE, as.is=TRUE)
 d$cname[d$cname == 'United Kingdom'] <- 'UK'
 d$cname[d$cname == 'United States'] <- 'USA'
 names(d)[3] <- 'region'
-
-## bin the counts
 d$count <- cut(d$count, breaks=c(0, 5, 10, 20, 30, 60))
+cut.labs <- c('< 5', '5 - 10', '10 - 20', '20 - 30', '> 30')
+
+## cumulative counts may be better
+## d <- do.call(rbind,by(d, d$region, function(rows) {
+##   rows$count <- cumsum(rows$count)
+##   return(rows)
+## }))
+## d$count <- cut(d$count, breaks=c(0, 5, 10, 40, 60, 100, 200))
+## cut.labs <- c('< 5', '5 - 10', '10 - 40', '40 - 60', '100 - 200', '> 200')
+
 by(d, d$year, function(rows) {
   world <- map_data('world')
   m <- merge(world, rows[c('region', 'count')], by='region', all.x=TRUE)
   m <- m[order(m$order),]
   ggplot(m, aes(long, lat, group=group))+geom_polygon(aes(fill=count))+
-    scale_fill_brewer(na.value='grey', drop=FALSE, labels=c('< 5', '5 - 10', '10 - 20', '20 - 30', '> 30'))+
+    scale_fill_brewer(na.value='grey', drop=FALSE, labels=cut.labs)+
       theme(axis.ticks = element_blank(),
             axis.text=element_blank(),
             axis.title=element_blank())+
